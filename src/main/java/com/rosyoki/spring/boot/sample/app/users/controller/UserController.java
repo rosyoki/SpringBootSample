1d52533e1715f1ac961925d24a332ecea02935e9
hirofumi_tsutsui <hirofumi.tsutsu@rosyoki.com>
2014-12-20 11:16:57 +0900

    パッケージの変更

---
+++ src/main/java/com/rosyoki/spring/boot/sample/app/users/controller/UserController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.rosyoki.spring.boot.sample.app.users.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author hirofumi_tsutsui
+ *
+ */
+@Controller
+public class UserController {
+    
+    @RequestMapping("/user/userList")
+    @ResponseBody
+    public String userList() {
+        return "userList";
+    }
+    
+    @RequestMapping("/user/regist")
+    public String registUser() {
+        return "bbbbbbb";
+    }
+}
