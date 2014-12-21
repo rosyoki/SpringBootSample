abb46308876943cefad4ae23e2bdfbadf9243efd
hirofumi_tsutsui <hirofumi.tsutsu@rosyoki.com>
2014-12-21 19:11:47 +0900

    cssファイル追加

--- src/main/java/com/rosyoki/spring/boot/sample/app/users/controller/UsersController.java
+++ src/main/java/com/rosyoki/spring/boot/sample/app/users/controller/UsersController.java
@@ -9,10 +9,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.rosyoki.spring.boot.sample.app.users.entity.Users;
-import com.rosyoki.spring.boot.sample.app.users.repository.UsersRepository;
 import com.rosyoki.spring.boot.sample.app.users.service.UsersService;
 
 /**
@@ -28,12 +25,11 @@ public class UsersController {
     private Logger logger = Logger.getLogger(UsersController.class);
 
     @RequestMapping("/user/userList")
-    @ResponseBody
     public String userList() {
         List<Users> usersList = usersService.findAll();
         logger.info(">>>>> " + usersList.size());
 
-        return "userList";
+        return "users/usersList";
     }
 
     @RequestMapping("/users/regist")
