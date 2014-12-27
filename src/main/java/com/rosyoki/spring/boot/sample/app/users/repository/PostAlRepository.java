190b1993c57f541b7d2ee9f23ec6b26aa093f844
hirofumi_tsutsui <hirofumi.tsutsu@rosyoki.com>
2014-12-27 10:03:06 +0900

    郵便番号検索サービスモジュール修正 ログバック設定

--- src/main/java/com/rosyoki/spring/boot/sample/app/users/repository/PostAlRepository.java
+++ src/main/java/com/rosyoki/spring/boot/sample/app/users/repository/PostAlRepository.java
@@ -1,8 +1,11 @@
 package com.rosyoki.spring.boot.sample.app.users.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.rosyoki.spring.boot.sample.app.users.entity.PostZipData;
 
+@Repository
 public interface PostAlRepository extends JpaRepository<PostZipData, Long>{
 
 }
