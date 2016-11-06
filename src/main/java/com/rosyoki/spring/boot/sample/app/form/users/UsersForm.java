74ff5373432881b4e0cd68ab1b959584da4525fc
hirofumi_tsutsui <hirofumi.tsutsu@rosyoki.com>
2016-11-06 16:48:14 +0900

    文字列範囲チェック追加

--- src/main/java/com/rosyoki/spring/boot/sample/app/form/users/UsersForm.java
+++ src/main/java/com/rosyoki/spring/boot/sample/app/form/users/UsersForm.java
@@ -4,6 +4,7 @@
 package com.rosyoki.spring.boot.sample.app.form.users;
 
 import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Range;
 
 import lombok.Data;
 
@@ -17,6 +18,7 @@ public class UsersForm {
     private Long id;
     
     @NotEmpty
+    @Range(min=6, max=99)
     private String loginName;
     
     @NotEmpty
