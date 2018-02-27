bffb5ff37b76b4ec3149a8a13a2f9307658873d3
tsutsuihirofumi <h-tsutsui@cnt.biglobe.co.jp>
2018-02-27 17:05:38 +0900

    SpringApplicationBuilderを実装

--- src/main/java/com/rosyoki/spring/boot/sample/app/App.java
+++ src/main/java/com/rosyoki/spring/boot/sample/app/App.java
@@ -2,20 +2,24 @@ package com.rosyoki.spring.boot.sample.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 /**
  * Hello world!
- *
  */
 @ComponentScan
 @EnableAutoConfiguration
 @ImportResource("classpath:applicationContext.xml")
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App extends SpringBootServletInitializer {
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
     }
 }
