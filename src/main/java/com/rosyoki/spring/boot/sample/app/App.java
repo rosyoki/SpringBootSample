851b77181b74ba83698dbb85e5d9712504545dc9
tsutsuihirofumi <h-tsutsui@cnt.biglobe.co.jp>
2018-03-06 10:49:35 +0900

    spring-boot2.0化

--- src/main/java/com/rosyoki/spring/boot/sample/app/App.java
+++ src/main/java/com/rosyoki/spring/boot/sample/app/App.java
@@ -2,8 +2,6 @@ package com.rosyoki.spring.boot.sample.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
@@ -13,13 +11,8 @@ import org.springframework.context.annotation.ImportResource;
 @ComponentScan
 @EnableAutoConfiguration
 @ImportResource("classpath:applicationContext.xml")
-public class App extends SpringBootServletInitializer {
+public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(App.class);
     }
 }
