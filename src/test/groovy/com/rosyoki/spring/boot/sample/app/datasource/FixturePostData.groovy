d16c8f586722e39779b21f114f716f4a5194f59c
Hirofumi Tsutsui <h-tsutsui@cnt.biglobe.co.jp>
2019-03-27 15:27:15 +0900

    郵便番号から住所を検索するテスト実装

---
+++ src/test/groovy/com/rosyoki/spring/boot/sample/app/datasource/FixturePostData.groovy
@@ -0,0 +1,29 @@
+package com.rosyoki.spring.boot.sample.app.datasource
+
+import com.rosyoki.spring.boot.sample.app.domain.City
+import com.rosyoki.spring.boot.sample.app.domain.NewZip
+import com.rosyoki.spring.boot.sample.app.domain.OldZip
+import com.rosyoki.spring.boot.sample.app.domain.Postal
+import com.rosyoki.spring.boot.sample.app.domain.Pref
+import com.rosyoki.spring.boot.sample.app.domain.Town
+
+class FixturePostData {
+    static Postal[] get() {
+        [
+                new Postal(
+                        new NewZip("2420007"),
+                        new OldZip("242"),
+                        new Pref("神奈川県"),
+                        new City("大和市"),
+                        new Town("中央林間")
+                ),
+                new Postal(
+                        new NewZip("2420001"),
+                        new OldZip("242"),
+                        new Pref("神奈川県"),
+                        new City("大和市"),
+                        new Town("下鶴間")
+                )
+        ]
+    }
+}
